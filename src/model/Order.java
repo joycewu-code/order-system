14ccb91969ba31034a6f327e1cd315f8b2a97dfd
Joyce Wu <candy890120@gmail.com>
2026-04-18 13:57:50 +0800

    feat: 新增 Order 資料模型

---
+++ src/model/Order.java
@@ -0,0 +1,17 @@
+public class Order {
+    private int quantity;
+    private int price;
+
+    public Order(int quantity, int price) {
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
